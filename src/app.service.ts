91eb54cdd9b922899ad8dfa070fdaae176f2873f
Camilo Duran <camilod529@gmail.com>
2025-03-05 15:14:03 -0500

    Add POST endpoint to execute robot tests and remove obsolete test file

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { exec } from 'child_process';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  executeRobotTest(url: string): Promise<{ success: boolean; output: string }> {
+    return new Promise((resolve) => {
+      const command = `/home/camilo/code/robot-framework-test/venv/bin/python -m robot --variable "URL:${url}" /home/camilo/code/robot-framework-test/tests/`;
+
+      console.log(command);
+      exec(command, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${stderr}`);
+          return resolve({ success: false, output: stderr });
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+        return resolve({ success: true, output: stdout });
+      });
+    });
   }
 }
